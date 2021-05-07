120a24d5acb3ded4d1840c8d67c5e26d6228ea57
hajujang <haju.jang37@gmail.com>
2021-05-06 22:01:36 -0500

    Initial commit

--- netlify/functions/movies.js
+++ netlify/functions/movies.js
@@ -3,6 +3,7 @@ let csv = require('neat-csv')
 
 // allows us to read files from disk
 let fs = require('fs')
+const { stringify } = require('querystring')
 
 // defines a lambda function
 exports.handler = async function(event) {
@@ -19,29 +20,61 @@ exports.handler = async function(event) {
   console.log(moviesFromCsv)
 
   // 🔥 hw6: your recipe and code starts here!
+
+  // get the year of the movie fromt the querystring parameters
   let year = event.queryStringParameters.year
-  let genre = event.queryStringParameters.genre
   
+  // get the genre of the movie fromt the querystring parameters
+  let genre = event.queryStringParameters.genre
+
+  // conditional statement based on what the request is
+  // Return a warning statement if users don't put year and genre request
   if (year == undefined || genre == undefined) {
     return {
       statusCode: 200, // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
-      body: `Nope!` // a string of data
+      body: `Please include the year and the genre.` // a string of data
     }
   }
+
+  // Return a number of movies and the list of movies that fits the user request
+  // Create an object that includes the number of movies and an array of movies 
   else {
     let returnValue = {
       numResults: 0,
       movies: []
     }
 
+    // loop through all movie listings
     for (let i=0; i < moviesFromCsv.length; i++) {
+    
+      // store each listing in memory
+      let movieListing = moviesFromCsv[i]
+
+      // Create a new movie object containing the pertinent fields
+      let relevantData = {
+        Title: movieListing.primaryTitle,
+        Year: movieListing.startYear,
+        Genre: movieListing.genres,
+        // Runtime: movieListing.runtimeMinutes
+      }
+      
+      // Return only if the year and genre are the ones user requests. Also exclude movies with no runtimeMinutes 
+      if (movieListing.startYear == year && movieListing.genres == genre && movieListing.runtimeMinutes!==`\\N`){
+        
+        // add +1 if to the numResults if the year and the genre fit with the request
+        returnValue.numResults = returnValue.numResults+1 
+
+        // add the listings to the Array of movie listings to return
+        returnValue.movies.push(relevantData) 
+        
+      }
 
     }
-
+    
     // a lambda function returns a status code and a string of data
     return {
       statusCode: 200, // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
-      body: `Hello from the back-end!` // a string of data
+      body: JSON.stringify(returnValue) // a string of data
     }
   }
 }
